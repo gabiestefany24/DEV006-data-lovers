5a63f679d360da83d945944fccfc05c87ff2ad3d
Angélica Rincon <rinconmezangelica@gmail.com>
2023-03-17 21:37:12 -0600

    maquetacion html

--- src/main.js
+++ src/main.js
@@ -4,3 +4,17 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const section_characters = document.getElementById("section-characters");
+const section_home = document.getElementById("categories")
+const button_characters = document.getElementById("personajes")
+
+section_home.style.display = "block";
+section_characters.style.display = "none";
+
+button_characters.addEventListener("click", SectionCharacters);
+
+function SectionCharacters(){
+    section_home.style.display = "none";
+    section_characters.style.display = "block";
+}
