a1caefc5556e294013b82f3acc191d2aa9e52bf8
Gabriela Porras <gabriela24401@gmail.com>
2023-03-20 22:49:14 -0300

    Filtrado por casa

--- src/data.js
+++ src/data.js
@@ -1,8 +1,10 @@
 // estas funciones son de ejemplo
 
-export const filterObj = (categories) => {
-  var newArray = categories.filter((key) => key.house === "Gryffindor");
+export const filterObj = (categories,house) => {
+  var newArray = categories.filter((key) => key.house === house);
+ 
   return newArray;
+
 
 };
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import data from './data/harrypotter/data.js';
 
 const contenedortarjetas= document.getElementById("contenedorcards");
 const button_charactergriffindor= document.getElementById("charactergriffindor");
+const button_characterslytherin= document.getElementById("characterslytherin");
+const button_characterravenclaw= document.getElementById("characterravenclaw");
+const button_characterhufflepuff= document.getElementById("characterhufflepuff");
 const button_home =document.getElementById("inicio");
 const section_books= document.getElementById("section-books");
 const section_potions=document.getElementById("section-potions");
@@ -16,14 +19,18 @@ const button_characters = document.getElementById("personajes")
 const button_houses = document.getElementById("casas")
 const button_spell = document.getElementById("hechizos")
 const button_books = document.getElementById("libros");
-const button_potions=document.getElementById("pociones");
+const button_potions=document.getElementById("pociones"); 
 const potion1=document.getElementById("potion1-name");
 const potion2=document.getElementById("potion2-name");
 const potion_instructions = document.getElementById("potion-instructions");
 
 SectionHome();
 
+
 button_charactergriffindor.addEventListener("click", FilterCharacGriffindor);
+button_characterslytherin.addEventListener("click", FilterCharacSlytherin);
+button_characterravenclaw.addEventListener("click", FilterCharacRavenclaw);
+button_characterhufflepuff.addEventListener("click", FilterCharacHufflepuff);
 button_characters.addEventListener("click", SectionCharacters);
 button_books.addEventListener("click", SectionBooks);
 button_potions.addEventListener("click", SectionPotions);
@@ -40,22 +47,70 @@ function SectionHome(){
     section_potions.style.display="none";
 }
 
-function Showcharacters(arrayfiltrated){
+function ShowcharactersGryffindor(arrayfiltrated){
     arrayfiltrated.forEach((character) => {
-    let optioncharacter =  `<section class="characters-card1">
-
-    <div class="bottom-card">  
+    let optioncharacter =  
+    `<section class="characters-card1">
+     <div class="bottom-card">  
       <div class="nombresection">
         <h3>${character.name}</h3>
         <div class="escudo">
         <img src="data/Images/gryffindor.png" width="90px" height="84px">
         </div> 
       </div>
-      <img id="harry" src="data/Images/harrypotter_cha.png" width="144.47px" height="213.94px">
-    </div>
-  </section> `   
+     </div>
+    </section> `   
   contenedortarjetas.innerHTML += optioncharacter;
 })}
+
+function ShowcharactersSlytherin(arrayfiltrated){
+  arrayfiltrated.forEach((character) => {
+  let optioncharacter =  
+  `<section class="characters-card2">
+  <div class="bottom-card">  
+    <div class="nombresection">
+      <h3>${character.name}</h3>
+      <div class="escudo">
+      <img src="data/Images/slytherin (6).png" width="90px" height="84px">
+      </div> 
+    </div>
+  </div>
+</section> `   
+contenedortarjetas.innerHTML += optioncharacter;
+})}
+
+function ShowcharactersRavenclaw(arrayfiltrated){
+  arrayfiltrated.forEach((character) => {
+  let optioncharacter =  
+  `<section class="characters-card3">
+   <div class="bottom-card">  
+    <div class="nombresection">
+      <h3>${character.name}</h3>
+      <div class="escudo">
+       <img src="data/Images/RAVENCLAW.png" width="90px" height="84px">
+      </div> 
+    </div>              
+ </div>
+ </section> ` 
+contenedortarjetas.innerHTML += optioncharacter;
+})} 
+
+
+function ShowcharactersHufflepuff(arrayfiltrated){
+  arrayfiltrated.forEach((character) => {
+  let optioncharacter =
+   `<section class="characters-card5">
+    <div class="bottom-card">  
+     <div class="nombresection">
+        <h3>${character.name}</h3>
+        <div class="escudo">
+          <img src="data/Images/HUFFLEPUFF.png" width="90px" height="84px">
+        </div> 
+     </div>
+    </div>
+    </section>`
+contenedortarjetas.innerHTML += optioncharacter;
+})} 
 
 
 function SectionCharacters(){
@@ -65,16 +120,108 @@ function SectionCharacters(){
     section_houses.style.display = "none";
     section_home.style.display = "none";
     section_characters.style.display = "block";
+    contenedortarjetas.innerHTML=`<section class="characters-card1">
+
+    <div class="bottom-card">  
+      <div class="nombresection">
+        <h3>HARRY <br />POTTER</h3>
+        <div class="escudo">
+        <img src="data/Images/gryffindor.png" width="90px" height="84px">
+        </div> 
+      </div>
+      <img id="harry" src="data/Images/harrypotter_cha.png" width="144.47px" height="213.94px">
+    </div>
+  </section>
+
+  <section class="characters-card2">
+    <div class="bottom-card">  
+      <div class="nombresection">
+        <h3>DRACO <br />MALFOY</h3>
+        <div class="escudo">
+        <img src="data/Images/slytherin (6).png" width="90px" height="84px">
+        </div> 
+      </div>
+      <img id="harry" src="data/Images/character_5.png" width="144.47px" height="213.94px">
+    </div>
+  </section>
+
+  <section class="characters-card3">
+    <div class="bottom-card">  
+      <div class="nombresection">
+        <h3>LUNA<br/>LOVEGOOD</h3>
+        <div class="escudo">
+        <img src="data/Images/RAVENCLAW.png" width="90px" height="84px">
+        </div> 
+      </div>
+      <img id="harry" src="data/Images/pngegg (2).png" width="144.47px" height="213.94px">
+    </div>
+  </section>
+
+  <section class="characters-card4">
+    <div class="bottom-card">  
+      <div class="nombresection">
+        <h3>NEVILLE<br/> LONGBOTTOM</h3>
+        <div class="escudo">
+        <img src="data/Images/gryffindor.png" width="90px" height="84px">
+        </div> 
+      </div>
+      <img id="harry" src="data/Images/character_2.png" width="144.47px" height="213.94px">
+    </div>
+  </section>
+
+  <section class="characters-card5">
+    <div class="bottom-card">  
+      <div class="nombresection">
+        <h3>CEDRIC<br/>DIAGORY</h3>
+        <div class="escudo">
+        <img src="data/Images/HUFFLEPUFF.png" width="90px" height="84px">
+        </div> 
+      </div>
+      <img id="harry" src="data/Images/harry-potter-and-the-goblet-of-fire-cedric-diggory.png" width="144.47px" height="213.94px">
+    </div>
+  </section>
+
+  <section class="characters-card6">
+    <div class="bottom-card">  
+      <div class="nombresection">
+        <h3>HERMIONE<br/>GRANGER</h3>
+        <div class="escudo">
+        <img src="data/Images/gryffindor.png" width="90px" height="84px">
+        </div> 
+      </div>
+      <img id="harry" src="data/Images/pngwing.com.png" width="144.47px" height="213.94px">
+    </div>
+  </section>`
+
 }
 
 function FilterCharacGriffindor(){
-
-    
         console.log("funciona");
-        const objectfiltrated = filterObj(data.characters);
-        Showcharacters(objectfiltrated);
+        contenedortarjetas.innerHTML=""
+        const objectfiltrated = filterObj(data.characters, "Gryffindor");
+        ShowcharactersGryffindor(objectfiltrated);
       } 
 
+function FilterCharacSlytherin(){
+  console.log("funciona");
+  contenedortarjetas.innerHTML=""
+  const objectfiltrated = filterObj(data.characters, "Slytherin");
+  ShowcharactersSlytherin(objectfiltrated);
+}
+
+function FilterCharacRavenclaw(){
+  console.log("funciona");
+  contenedortarjetas.innerHTML=""
+  const objectfiltrated = filterObj(data.characters, "Ravenclaw");
+  ShowcharactersRavenclaw(objectfiltrated);
+}
+
+function FilterCharacHufflepuff(){
+  console.log("funciona");
+  contenedortarjetas.innerHTML=""
+  const objectfiltrated = filterObj(data.characters, "Hufflepuff");
+  ShowcharactersHufflepuff(objectfiltrated);
+}  
 
 
 function SectionHouses(){
